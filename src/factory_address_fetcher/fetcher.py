4d7dcb9c7f985ae3c96c1d48fac47c2c8483125c
moose-code <jonjonclark@gmail.com>
2024-03-25 15:18:12 +0200

    Update batch size params

--- src/factory_address_fetcher/fetcher.py
+++ src/factory_address_fetcher/fetcher.py
@@ -4,15 +4,15 @@ import asyncio
 
 async def collect_events():
     # choose network
-    client = hypersync.HypersyncClient()
+    client = hypersync.HypersyncClient("https://polygon.hypersync.xyz")
 
     query = hypersync.Query(
         from_block=0,
         # Select the logs we want
         logs=[LogSelection(
-            address=["0x1F98431c8aD98523631AE4a59f267346ea31F984", "0x0bfbcf9fa4f9c56b0f40a671ad40e0805a091865", "0xbaceb8ec6b9355dfc0269c18bac9d6e2bdc29c4f"], # uniswap factory
+            address=["0x1F98431c8aD98523631AE4a59f267346ea31F984"], # uniswap factory
             topics=[["0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118"]],
-        )],
+        )], 
         # Select the fields and tables we want
         field_selection=FieldSelection(
             log=[
@@ -32,14 +32,16 @@ async def collect_events():
 
     config = hypersync.ParquetConfig(
         path="data",
-        hex_output=True,
+        hex_output=True, 
+        batch_size=1000000,
+        concurrency=10,
         # column_mapping=ColumnMapping(
         #     # map value columns to float so we can do calculations with them
         #     decoded_log={
         #         "pool": DataType.FLOAT64,
         #     },
         #     transaction={
-        #         TransactionField.GAS_USED: DataType.FLOAT64,
+        #         TransactionField.BLOCK_NUMBER: DataType.,
         #     },
         # ),
         # give event signature so client can decode logs into decoded_logs.parquet file
